caf70e1f9dd46bb8eb2896bed7e0a1aa89b3c50f
alejan01s <alejan01si@gmail.com>
2016-12-11 15:44:31 -0500

    v i never update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousV1.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.adafruit.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,7 +17,21 @@ import java.util.concurrent.TimeUnit;
 @Autonomous (name = "AutonomousV1", group = "Sensor")
 public class AutonomousV1 extends DefinerClass {
 
+    public double EncoderClicks = 2510;
+    public boolean shoot = false;
+    public boolean pause = false;
+    public boolean resume = false;
+    public boolean seclaunch = false;
+
+    public DcMotor LauncherM;
+    public Servo Reloader;
+
     public void initializeRobot () {
+        LauncherM = hardwareMap.dcMotor.get("Launcher");
+        Reloader = hardwareMap.servo.get("Reloader");
+
+        LauncherM.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         super.initializeRobot();
     }
 
@@ -32,102 +49,176 @@ public class AutonomousV1 extends DefinerClass {
         int Angle1 = 270;
         int Angle2 = 90;
         int Angle3 = 180;
-        int FinalAngle = 90;
-        int NumberOfRevs1 = 10;
-        int NumberOfRevs2 = 5;
-        int NumberOfRevsFinal = 5;
+        int FinalAngle = 55;
+        int NumberOfRevs1 = -300;
+        int NumberOfRevs2 = -320;
+        int NumberOfRevsFinal = -3500;
         waitForStart();
         while(opModeIsActive()){
+
+            telemetry.addData("Encoder Clicks: ", LauncherM.getCurrentPosition());
+
+            telemetry.addData("FL: ", FL.getCurrentPosition());
+            telemetry.addData("FR: ", FR.getCurrentPosition());
+            telemetry.addData("BL: ", BL.getCurrentPosition());
+            telemetry.addData("BR: ", BR.getCurrentPosition());
+
+            telemetry.addData("x", x);
+
+            telemetry.update();
             //Step 1: Position Robot
-            if(step == 0) {
-                super.runOpMode(Angle1, false, false);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
-            }
-            if(step == 1){
-                if(FR.getCurrentPosition() < NumberOfRevs1) {
-                    BL.setPower(.5);
-                    BR.setPower(.5);
-                    FR.setPower(.5);
-                    FL.setPower(.5);
+            //if(step == 0) {
+                //super.runOpMode(Angle1, false, false);
+                //TimeUnit.SECONDS.sleep(1);
+                //step = step++;
+            //}
+            if(step == 0){
+                if(FR.getCurrentPosition() > NumberOfRevs1) {
+                    BL.setPower(-.5);
+                    BR.setPower(-.5);
+                    FR.setPower(-.5);
+                    FL.setPower(-.5);
                 }
                 else{
                     BL.setPower(0);
                     BR.setPower(0);
                     FR.setPower(0);
                     FL.setPower(0);
-                    step = step++;
+                    step = step+1;
                 }
             }
-            if(step == 2){
-                super.runOpMode(Angle2, false, false);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
-            }
+            //if(step == 2){
+                //super.runOpMode(FinalAngle, false, false);
+                //TimeUnit.SECONDS.sleep(2);
+                //step = step+1;
+            //}
+            //if(step == 2){
+                //super.runOpMode(Angle2, false, false);
+                //TimeUnit.SECONDS.sleep(1);
+                //step = step+1;
+            //}
             //Launch ball
-            if(step == 3){
-                super.runOpMode(Angle2, true, false);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
-            }
+            //if(step == 1){
+                //LaunchSequence();
+                //TimeUnit.SECONDS.sleep(3);
+                //step = step+1;
+            //}
             //Reload ball
-            if(step == 4){
-                super.runOpMode(Angle2, false, true);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
-            }
+            //if(step == 4){
+                //super.runOpMode(Angle2, false, true);
+                //TimeUnit.SECONDS.sleep(1);
+                //step = step++;
+            //}
             //Launch second ball
-            if(step == 5){
-                super.runOpMode(Angle2, true, false);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
+            if(step == 1){
+                shoot = true;
+                step = step+1;
             }
-            if(step == 6){
-                super.runOpMode(Angle3, false, false);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
-            }
-            if(step == 7){
-                if(FR.getCurrentPosition() < NumberOfRevs2){
-                    FR.setPower(.5);
-                    FL.setPower(.5);
-                    BR.setPower(.5);
-                    BL.setPower(.5);
+            //if(step == 5){
+                //super.runOpMode(Angle3, false, false);
+                //TimeUnit.SECONDS.sleep(2);
+                //step = step+1;
+            //}
+            /*if(step == 2){
+                Thread.sleep(4000);
+                if(FR.getCurrentPosition() > NumberOfRevs2){
+                    FR.setPower(-.5);
+                    FL.setPower(-.5);
+                    BR.setPower(-.5);
+                    BL.setPower(-.5);
                 }
                 else{
                     FR.setPower(0);
                     FL.setPower(0);
                     BR.setPower(0);
                     BL.setPower(0);
-                    step = step++;
+                    step = step+1;
                 }
-            }
-            //LAST ORIENTATION
-            if(step == 8){
-                super.runOpMode(FinalAngle, false, false);
-                TimeUnit.SECONDS.sleep(1);
-                step = step++;
-            }
-            //FINAL MOVE
-            if(step == 9){
-                if(FR.getCurrentPosition() < NumberOfRevsFinal){
-                    FR.setPower(.5);
-                    FL.setPower(.5);
-                    BR.setPower(.5);
-                    BL.setPower(.5);
-                }
-                else{
-                    FR.setPower(0);
-                    FL.setPower(0);
-                    BR.setPower(0);
-                    BL.setPower(0);
-                    step = step++;
+            }*/
+
+            if(step == 2){
+                if(!shoot) {
+                    shoot = true;
+                    step = step + 1;
                 }
             }
 
-            telemetry.addData("x", x);
-            telemetry.update();
+            if(step == 3){
+                if(!shoot) {
+                    if (FR.getCurrentPosition() > NumberOfRevsFinal) {
+                        FR.setPower(-.5);
+                        FL.setPower(-.5);
+                        BR.setPower(-.5);
+                        BL.setPower(-.5);
+                    } else {
+                        FR.setPower(0);
+                        FL.setPower(0);
+                        BR.setPower(0);
+                        BL.setPower(0);
+                        step = step + 1;
+                    }
+                }
+            }
+
+            if(shoot) {
+                if(!resume) {
+                    if (LauncherM.getCurrentPosition() <= 400 + (EncoderClicks - 2510)) {
+                        LauncherM.setPower(1);
+                    } else if (LauncherM.getCurrentPosition() <= 525 + (EncoderClicks - 2510)) {
+                        LauncherM.setPower(.08);
+                    }
+                    else{
+                        pause = true;
+                    }
+                    if (pause) {
+                        //INPUT RELOAD FUNCTION WHEN READY HERE
+                        LauncherM.setPower(0.03);
+                        Reloader.setPosition(256);
+                        resume = true;
+                        pause = false;
+                    }
+                }
+                if(resume) {
+                    if (LauncherM.getCurrentPosition() > 550 + (EncoderClicks-2510) && LauncherM.getCurrentPosition() <= 1000 + (EncoderClicks - 2510)) {
+                        LauncherM.setPower(.08);
+                    }
+                    else if (LauncherM.getCurrentPosition() > 1000 + (EncoderClicks-2510) && LauncherM.getCurrentPosition() <= 1800 + (EncoderClicks - 2510)) {
+                        LauncherM.setPower(1);
+                    } else if (LauncherM.getCurrentPosition() > 1800 + (EncoderClicks - 2510) && LauncherM.getCurrentPosition() <= EncoderClicks) {
+                        LauncherM.setPower(.08);
+                    } else {
+                        LauncherM.setPower(0);
+                        Reloader.setPosition(0);
+                        resume = false;
+                        EncoderClicks = EncoderClicks + 2510;
+                        Thread.sleep(1000);
+                        shoot = false;
+                    }
+                }
+            //if(step == 5){
+                //super.runOpMode(0, false, false);
+                //TimeUnit.SECONDS.sleep(2);
+                //step = step+1;
+            //}
+            //FINAL MOVE
+            /*if(step == 6){
+                if(FR.getCurrentPosition() > NumberOfRevsFinal){
+                    FR.setPower(-.5);
+                    FL.setPower(-.5);
+                    BR.setPower(-.5);
+                    BL.setPower(-.5);
+                }
+                else{
+                    FR.setPower(0);
+                    FL.setPower(0);
+                    BR.setPower(0);
+                    BL.setPower(0);
+                    step = step+1;
+                }
+            }*/
             idle();
         }
+    }
+
     }
 }
