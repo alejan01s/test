189e3c6a2b34d58b619ced8b1f527b4e784a161d
alejan01s <alejan01si@gmail.com>
2016-12-28 14:16:33 -0500

    v i never update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorTester.java
@@ -1,17 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 /**
  * Created by aleja on 12/26/2016.
  */
 
 @Autonomous(name = "ColorSensorTester", group = "Sensor")
-public class ColorTester extends DefinerClass {
+public class ColorTester extends LinearOpMode {
 
-    @Override
+    public ColorSensor colorReader;
+
+    public boolean isRed;
+    public boolean isBlue;
+
     public void initializeRobot(){
-        super.initializeRobot();
+        colorReader = hardwareMap.colorSensor.get("color");
     }
 
     @Override
@@ -22,7 +28,37 @@ public class ColorTester extends DefinerClass {
         waitForStart();
         while(opModeIsActive()){
 
+            telemetry.addData("Red: ", colorReader.red());
+            telemetry.addData("Blue: ", colorReader.blue());
+            telemetry.addData("Green: ", colorReader.green());
+            telemetry.addData("Alpha: ", colorReader.alpha());
+            telemetry.addData("RGBA: ", colorReader.argb());
+
+            telemetry.addData("isRed: ", isRed);
+            telemetry.addData("isBlue: ", isBlue);
+
+            telemetry.update();
+
+            isRed = colorReader.red() > 1 && colorReader.red() > colorReader.blue() ? true : false;
+            isBlue = colorReader.blue() > 1 && colorReader.blue() > colorReader.red() ? true : false;
+
+            if(isRed) {
+                redProcedure();
+            }
+
+            if(isBlue){
+                blueProcedure();
+            }
+
             idle();
         }
+    }
+
+    public void redProcedure () {
+        //RUN BUTTON PUSHER AND POSITION FOR RED
+    }
+
+    public void blueProcedure () {
+        //RUN BUTTON PUSHER AND POSITION FOR BLUE 
     }
 }
